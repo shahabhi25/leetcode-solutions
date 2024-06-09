954e6473e20a3f193577cbc3c4eb0c5cc863dc2a
Abhishek Shah <abhisheksmith02@gmail.com>
2024-06-09 15:56:15 +0530

    Time: 287 ms (23.98%), Space: 303.8 MB (10.09%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,43 +2,34 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n=s.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        int ans = 1;
-        int ans_x{0};
-        int ans_y{0};
-        for(int i=0;i<n; i++)
+        if(n==0) return "";
+        vector<vector<int>> dp(n, vector<int>(n));
+        string ans="";
+        ans+=s[0];
+        int largest_palindrome=1;
+        
+        for(int i=0; i<n; i++)
         {
             for(int j=0; j<n-i; j++)
             {
-                int x = j;
-                int y = j+i;
+                int x=j;
+                int y=j+i;
                 if(x==y) dp[x][y]=1;
-                else if(y-x==1)
+                else if(y-x==1) 
                 {
-                    if(s[x]==s[y])
-                    {
-                        dp[x][y]=1;
-                        ans_x=x;
-                        ans_y=y;
-                        ans=max(ans, y-x+1);
-                    }
-                    else dp[x][y]=0;
+                    dp[x][y] = (s[x]==s[y])?1:0;
                 }
                 else
                 {
                     dp[x][y] = dp[x+1][y-1] && (s[x]==s[y]);
-                    if(dp[x][y]==1)
-                    {
-                        if (y-x+1>ans)
-                        {
-                            ans_x=x;
-                            ans_y=y;
-                            ans=y-x+1;
-                        }
-                    }
+                }
+                if(dp[x][y]==1 && (y-x+1)>largest_palindrome)
+                {
+                    largest_palindrome = (y-x+1);
+                    ans = s.substr(x, largest_palindrome);
                 }
             }
         }
-        return s.substr(ans_x, ans);
+        return ans;
     }
 };
