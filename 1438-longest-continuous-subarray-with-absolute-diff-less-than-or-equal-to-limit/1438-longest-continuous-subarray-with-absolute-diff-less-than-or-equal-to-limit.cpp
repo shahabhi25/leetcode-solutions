c4701a4ecefc7dd65bfa23e15cab14691ff69d5f
Abhishek Shah <abhisheksmith02@gmail.com>
2024-06-23 16:34:11 +0530

    Time: 102 ms (55.02%), Space: 65.1 MB (48.10%) - LeetHub

---
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        int n=nums.size();
+        int ans=nums[0]>limit?1:0;
+        map<int, int> mp;
+        // mp[nums[0]]=1;
+        
+        int l=0;
+        int r=0;
+        while(r<n)
+        {
+            mp[nums[r]]++;
+            while(l<r && mp.rbegin()->first-mp.begin()->first > limit) 
+            {
+                mp[nums[l]]--;
+                if(mp[nums[l]]==0) mp.erase(nums[l]);
+                l++;
+            }
+            
+            ans = max(ans, r-l+1);
+            r++;
+        }
+        return ans;
+    }
+};
